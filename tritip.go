fdd4479a4440a0cc5573a2454504f75e1cc21035
Porter Road <porterroad@Porters-MBP.attlocal.net>
2022-05-16 14:23:42 -0500

    get list works, added gitignore

--- tritip.go
+++ tritip.go
@@ -2,21 +2,56 @@ package main
 
 import (
 	data "ajl/tritip/data"
+	"encoding/json"
 	"fmt"
-
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
-
 	"strings"
 
 	"github.com/gocarina/gocsv"
 )
 
-func getOrders() {
+func getOrders() (data.ListResponse, error) {
 	// get orders for update
+	client := &http.Client{}
+	orders := data.ListResponse{}
+	key, secret := data.GetApiSecret()
+
+	req, err := http.NewRequest(http.MethodGet, "https://ssapi.shipstation.com/orders?orderStatus=awaiting_shipment", nil)
+
+	req.SetBasicAuth(key, secret)
+
+	fmt.Printf("req: %v\n", req)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+		fmt.Printf("request error: %v\n", err)
+	}
+
+	respJSON, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("read i/o error ::", err)
+	}
+
+	err = json.Unmarshal([]byte(respJSON), &orders)
+	fmt.Printf("orders: %v\n", orders)
+	if err != nil {
+		fmt.Printf("json unmarshalling error :: %v\n", err)
+	}
+
+	fmt.Printf("resp: %v", resp)
+
+	defer resp.Body.Close()
+
+	return orders, err
 }
 
 func orderUpdate(o []*data.OrderRecordInput) {
 	// Shipstation API Call
+	fmt.Printf("order update %v\n", o)
 }
 
 func csvReader(s string) ([]*data.OrderRecordInput, error) {
@@ -52,4 +87,6 @@ func initializeCSV() {
 func main() {
 
 	initializeCSV()
+
+	getOrders()
 }
